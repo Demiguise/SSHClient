81ad5b7aaa3455bf612c2035ecf98dcf81594ffd
Andy <andy.c.hewitson@gmail.com>
2020-06-03 15:46:46 +0200

    Initial sketch of SSHClient API

--- include/ssh.h
+++ include/ssh.h
@@ -1,16 +1,30 @@
-#ifndef __SSHC_H__
-#define __SSHC_H__
+#ifndef __SSH_H__
+#define __SSH_H__
 
-/*
-*/
-class SSHC
-{
-  int a;
-};
+#include <functional>
+#include <memory>
+#include <optional>
 
 namespace SSH
 {
-  SSHC Connect();
+  using TCtx = std::weak_ptr<void>;
+  using TSendFunc = std::function<void (TCtx ctx, const char* pBuf, const int bufLen)>;
+  using TRecvFunc = std::function<void (TCtx ctx, const char *pBuf, const int bufLen)>;
+
+  class Client
+  {
+  private:
+    TSendFunc mSendFunc;
+    TRecvFunc mRecvFunc;
+    TCtx mCtx;
+
+  public:
+    //Base constructor
+    Client(TSendFunc sendFunc, TRecvFunc recvFunc, TCtx ctx);
+
+    void Send(const char* pBuf, const int bufLen);
+    void Recv(const char* pBuf, const int bufLen);
+  };
 }
 
-#endif //~__SSHC_H__
+#endif //~__SSH_H__
--- src/ssh.cpp
+++ src/ssh.cpp
@@ -1,6 +1,21 @@
 #include "ssh.h"
 
-SSHC SSH::Connect()
+using namespace SSH;
+
+Client::Client(TSendFunc sendFunc, TRecvFunc recvFunc, TCtx ctx)
+  : mSendFunc(sendFunc)
+  , mRecvFunc(recvFunc)
+  , mCtx(ctx)
 {
-  return SSHC();
+
+}
+
+void Client::Send(const char* pBuf, const int bufLen)
+{
+  mSendFunc(mCtx, pBuf, bufLen);
+}
+
+void Client::Recv(const char* pBuf, const int bufLen)
+{
+  mRecvFunc(mCtx, pBuf, bufLen);
 }
