05ca14a06585c37b8696fc8bedeaee0b87c37416
Andy <andy.c.hewitson@gmail.com>
2020-06-03 12:28:37 +0200

    Commit sources and scripts

---
+++ src/include/ssh.h
@@ -0,0 +1,16 @@
+#ifndef __SSHC_H__
+#define __SSHC_H__
+
+/*
+*/
+class SSHC
+{
+  int a;
+};
+
+namespace SSH
+{
+  SSHC Connect();
+}
+
+#endif //~__SSHC_H__
---
+++ src/ssh.cpp
@@ -0,0 +1,6 @@
+#include "ssh.h"
+
+SSHC SSH::Connect()
+{
+  return SSHC();
+}
