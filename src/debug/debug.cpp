597f49f634e9af7bf8c72fe955416ed3229ff635
Andy <andy.c.hewitson@gmail.com>
2020-06-22 15:54:08 +0200

    Added file writing for dumping buffers

--- src/debug/debug.cpp
+++ src/debug/debug.cpp
@@ -1,9 +1,28 @@
 #include "debug.h"
 #include <stdio.h>
+#include <fstream>
+#include <iomanip>
 
 using namespace SSH;
 
 void SSH::DumpBufferToDisk(std::string fileName, const Byte* pBuf, const int bufLen)
 {
+  std::ofstream ofs("dump/" + fileName);
 
+  constexpr int columnLimit = 16;
+  ofs << "Len: " << bufLen << std::endl;
+  ofs << "Bytes : [" << std::endl << '\t' << std::hex;
+
+  //Now print each byte in hexadecimal form
+  for (int i = 0; i < bufLen ; ++i)
+  {
+    if ((i != 0 ) && ((i % columnLimit) == 0))
+    {
+      ofs << std::endl << '\t';
+    }
+
+    ofs << std::setfill('0') << std::setw(2) << (int)pBuf[i] << ", ";
+  }
+
+  ofs << std::endl << "]" << std::endl;
 }
