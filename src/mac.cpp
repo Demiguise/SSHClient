98e9b33b3447a76ed6bd854e315e2789f0da783a
Andy <andy.c.hewitson@gmail.com>
2020-06-25 11:49:56 +0200

    Added stub for HMAC_SHA2_256 MAC Handler

--- src/mac.cpp
+++ src/mac.cpp
@@ -35,10 +35,40 @@ public:
   virtual MACHandlers Type() override { return MACHandlers::None; }
 };
 
+class HMAC_SHA2_256_MACHandler : public IMACHandler
+{
+public:
+  HMAC_SHA2_256_MACHandler() = default;
+
+  virtual UINT32 Len() override
+  {
+    return 0;
+  }
+
+  virtual bool SetKey(const Key& macKey, const Key& ivKey) override
+  {
+    return true;
+  }
+
+  virtual bool Create(TPacket pPacket) override
+  {
+    return true;
+  }
+
+  virtual bool Verify(TPacket pPacket) override
+  {
+    return true;
+  }
+
+  virtual MACHandlers Type() override { return MACHandlers::HMAC_SHA2_256; }
+};
+
 TMACHandler MAC::Create(MACHandlers handler)
 {
   switch (handler)
   {
+    case MACHandlers::HMAC_SHA2_256:
+      return std::make_shared<HMAC_SHA2_256_MACHandler>();
     default:
     case MACHandlers::None:
       return std::make_shared<None_MACHandler>();
